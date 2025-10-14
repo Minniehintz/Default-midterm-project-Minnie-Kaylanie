4523560df2b8b96e89e8bdfd398df722d4dad399
Minnie <minniehintz02@gmail.com>
2025-10-14 15:11:10 -0500

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,26 @@
+﻿class books
+{
+    // Properties
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Genre { get; set; }
+    public int PageLength { get; set; }
+    public int YearPublished { get; set; }
+    public bool IsCheckedOut { get; set; }
+
+    public books(string title, string author, string genre, int pages, int year, bool isCheckedOut)
+    {
+        Title = title;
+        Author = author;
+        Genre = genre;
+        PageLength = pages;
+        YearPublished = year;
+        IsCheckedOut = isCheckedOut;
+    }
+
+    public override string ToString()
+    {
+        return $"{Title} by {Author} | {Genre} | {PageLength} pages | {YearPublished} | " +
+               (IsCheckedOut ? "Checked Out" : "Available");
+    }
+}
---
+++ Library.cs
@@ -0,0 +1,15 @@
+﻿
+public static class Books
+{
+    private static int Main(string[] args)
+    {
+        String Title;
+        String Author;
+        String Genre;
+        int PageLength;
+        int YearPublished;
+        bool IsCheckedOut;
+
+        //invoke .ToString() to format book info for printing
+    }
+}
